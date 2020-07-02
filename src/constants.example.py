d8639397020071140aa1a52cf2641db334c065bc
Kevin Paulo <kevinppaulo@gmail.com>
2020-07-02 14:05:50 -0300

    [wip] fixing unexistent folder error

--- src/constants.example.py
+++ src/constants.example.py
@@ -1,18 +1,17 @@
 # SERVER DOMAIN ETC.
-domain_name                         = "testechat.eth0.net.br"
+domain_name                         = "your-domain-name.com"
 
 # CHAT API ENDPOINTS
-CHAT_API_URL                        = 'https://eu200.chat-api.com/instance126385'
-INSTANCE_ID                         = '126385'
-CHAT_API_TOKEN                      = '0k4xuq99kucdtnqu'
-CHAT_API_TOKEN_MD5                  = '4abc11d67bbc8634a2d7f3ca4c9ae429'
+CHAT_API_URL                        = 'https://eu200.chat-api.com/instanceXXXXXX'
+INSTANCE_ID                         = 'XXXXXX'
+CHAT_API_TOKEN                      = 'your-api-token'
 
 # check https://chat-api.com/en/docs_beta.html#/messages/getFile for more.
 CHAT_GET_FILE                       = "/getFile?msgId={}&token={}"
 
 
 # ROCKET CHAT ENDPOINTS
-ROCKET_URL_PREFIX                   = 'http://localhost'
+ROCKET_URL_PREFIX                   = 'http://your-rocket-chat-domain'
 ROCKET_MESSAGE_URL_POSTFIX          = '/api/v1/livechat/message'
 ROCKET_GET_ROOM_POSTFIX             = '/api/v1/livechat/room'
 ROCKET_VISITOR_POSTFIX              = "/api/v1/livechat/visitor"
